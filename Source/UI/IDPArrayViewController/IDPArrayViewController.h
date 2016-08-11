993bd03a79af2d461205570fca909e4e7ee09c60
Ievgen Gavrysh <igavrysh@gmail.com>
2016-08-11 18:53:04 +0300

    add array view controller

---
+++ Source/UI/IDPArrayViewController/IDPArrayViewController.h
@@ -0,0 +1,13 @@
+//
+//  IDPArrayViewController.h
+//  SuperUI
+//
+//  Created by Ievgen on 8/11/16.
+//  Copyright © 2016 1mlndollarsasset. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDPArrayViewController : UIViewController
+
+@end
---
+++ Source/UI/IDPArrayViewController/IDPArrayViewController.m
@@ -0,0 +1,28 @@
+//
+//  IDPArrayViewController.m
+//  SuperUI
+//
+//  Created by Ievgen on 8/11/16.
+//  Copyright © 2016 1mlndollarsasset. All rights reserved.
+//
+
+#import "IDPArrayViewController.h"
+
+@interface IDPArrayViewController ()
+
+@end
+
+@implementation IDPArrayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
